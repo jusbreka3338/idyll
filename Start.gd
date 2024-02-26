fbdb02a53f6a52013dc49e1b7736c2a74f8d4581
Lachly <60305985+Lachly@users.noreply.github.com>
2024-02-26 10:48:07 -0700

    mainmenu stuff

---
+++ Start.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _ready():
+	
+	$AnimationPlayer.play("Fade In")
+	await get_tree().create_timer(6).timeout
+	$AnimationPlayer.play("Fade Out")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://main_menu.tscn")
---
+++ main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+@export var mainGameScene : PackedScene
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
