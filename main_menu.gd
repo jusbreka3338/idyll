7accf7c7afc7358c20eb366efbb5364f218f9337
Lachly <60305985+Lachly@users.noreply.github.com>
2024-02-26 11:02:15 -0700

    stuff

--- main_menu.gd
+++ main_menu.gd
@@ -9,7 +9,7 @@ func _on_play_button_pressed():
 
 
 func _on_credits_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Credits.tscn")
 
 
 func _on_quit_button_pressed():
