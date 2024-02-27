9c30b2bafa5551c5275178b6c3f6dfbf70005c8b
Lachly <60305985+Lachly@users.noreply.github.com>
2024-02-27 10:14:59 -0700

    smooth

--- Start.gd
+++ Start.gd
@@ -7,3 +7,22 @@ func _ready():
 	$AnimationPlayer.play("Fade Out")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+func testEs():
+	if Input.is_action_just_pressed("skip"):
+		get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _process(delta):
+	testEsc()
+	
+	
+	
+	
+
+func skip():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+func testEsc():
+	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
+		skip()
