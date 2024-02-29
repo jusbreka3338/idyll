402c977ee114649edafca75e7cfdd157b74b7322
Peyton Brown <142298566+jusbreka3338@users.noreply.github.com>
2024-02-29 09:01:05 -0700

    pushed BAD CHANGES for Kesler
    pushing BAD CHANGES TO MAIN because kesler broke it :(

--- Scripts/TestMap.gd
+++ Scripts/TestMap.gd
@@ -4,6 +4,7 @@ var BOBspeed = 50
 var FREDspeed = 45
 var STACYspeed = 75
 var crowns = 0
+var stage = 1
 
 func _ready():
 	pass
@@ -12,11 +13,16 @@ func _process(delta):
 	$BobPath/BobFollow.progress += BOBspeed * delta
 	$FredPath/FredFollow.progress += FREDspeed * delta
 	$StacyPath/StacyFollow.progress += STACYspeed * delta
-	if crowns >= 500:
+	
+	if Input.is_action_pressed("cheat_crowns"):
+		global.Crowns += 10
+	
+	if crowns >= 500 and stage == 1:
 		var simultaneous_scene = preload("res://Scenes/Stage2.tscn").instantiate()
+		get_tree().root.add_child(simultaneous_scene)
+		stage = 2
+	
+
 
 func _add_a_scene_manually():
-	# This is like autoloading the scene, only
-	# it happens after already loading the main scene.
-	var simultaneous_scene = preload("res://Scenes/Stage2.tscn")
-	get_tree().root.add_child(simultaneous_scene)
+	pass
