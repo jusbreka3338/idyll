3e5506adef32233414968bb9cda03e7b26953ef2
Peyton Brown <142298566+jusbreka3338@users.noreply.github.com>
2024-02-29 08:46:28 -0700

    scene changing
    worked on changing scene

--- Scripts/TestMap.gd
+++ Scripts/TestMap.gd
@@ -3,6 +3,7 @@ extends Node2D
 var BOBspeed = 50
 var FREDspeed = 45
 var STACYspeed = 75
+var crowns = 0
 
 func _ready():
 	pass
@@ -11,3 +12,11 @@ func _process(delta):
 	$BobPath/BobFollow.progress += BOBspeed * delta
 	$FredPath/FredFollow.progress += FREDspeed * delta
 	$StacyPath/StacyFollow.progress += STACYspeed * delta
+	if crowns >= 500:
+		var simultaneous_scene = preload("res://Scenes/Stage2.tscn").instantiate()
+
+func _add_a_scene_manually():
+	# This is like autoloading the scene, only
+	# it happens after already loading the main scene.
+	var simultaneous_scene = preload("res://Scenes/Stage2.tscn")
+	get_tree().root.add_child(simultaneous_scene)
